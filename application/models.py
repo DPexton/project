c6f72d68f722b1e63e61a0af82f7d03a4a3e0592
DPexton <davidpj@hotmail.co.uk>
2021-05-13 10:32:56 +0000

    minor fixes 2

--- application/models.py
+++ application/models.py
@@ -2,15 +2,15 @@ from application import db
 from datetime import datetime
 
 class Teams(db.Model):
-    team_id = db.Column(db.Integer, primary_key=True)
-    team_name = db.Column(db.String(30), Nullable=False)
-    overall_skill = db.Column(db.Integer, Nullable=False)
-    date_formed = db(Column.Datetime, Nullable=False, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    team_name = db.Column(db.String(30), nullable=False)
+    overall_skill = db.Column(db.Integer, nullable=False)
+    date_formed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     players = db.relationship('Team', backref='player')
 
 class Players(db.Model):
-    player_id = db.Column(db.Integer, Primary_key=True)
-    player_name = db.column(db.String(50), Nullable=False)
-    position = db.Column(db.String(10), Nullable=False)
-    skill = db.Column(db.Integer, Nullable=False)
-    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(50), nullable=False)
+    position = db.Column(db.String(10), nullable=False)
+    skill = db.Column(db.Integer, nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
