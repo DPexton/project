27951651858b6af88ae0d3066c177a707e0f7708
DPexton <davidpj@hotmail.co.uk>
2021-05-13 10:13:48 +0000

    Commit to fix IP

---
+++ application/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
+
+class TeamForm(FlaskForm):
+    form_teamname = StringField("Please enter the name of your Team", validators=[DataRequired()])
+    form_city = StringField("Please enter the City your team is from", validators=[DataRequired()]) 
+    submit = SubmitField('create team')
+
+class PlayerForm(FlaskForm):
+    form_playername = StringField("Please Enter the full name of your player", validators=[DataRequired()])
+    form_playerskill = IntegerField("Please Enter the skill level of your player between 1 and 100", validators=[NumberRange(min=1, max=100, message="enter a number between 1 & 100")] )
+    form_position = SelectField("Please select your players position", choices=[
+        ("GK", "Goalkeeper"),
+        ("RB", "Right Back"),
+        ("LB", "Left Back"),
+        ("CB", "Center Back"),
+        ("RW", "Right Wing"),
+        ("LW", "Left Wing"),
+        ("CM", "Central Midfield"),
+        ("CAM", "Centre Attack Midfield"),
+        ("ST", "Striker")
+        ])
+    submit = SubmitField('create player')
+
+class UpdateTeam(FlaskForm):
+    form_teamname = StringField("Please enter the new name of your Team", validators=[DataRequired()])
+    form_city = StringField("Please enter the City your team is from", validators=[DataRequired()]) 
+    submit = SubmitField('create team')
+
+class UpdatePlayer(FlaskForm):
+    form_playername = StringField("Please Enter the full name of your player", validators=[DataRequired()])
+    form_playerskill = IntegerField("Please Enter the skill level of your player between 1 and 100", validators=[NumberRange(min=1, max=100, message="enter a number between 1 & 100")] )
+    form_position = SelectField("Please select your players position", choices=[
+        ("GK", "Goalkeeper"),
+        ("RB", "Right Back"),
+        ("LB", "Left Back"),
+        ("CB", "Center Back"),
+        ("RW", "Right Wing"),
+        ("LW", "Left Wing"),
+        ("CM", "Central Midfield"),
+        ("CAM", "Centre Attack Midfield"),
+        ("ST", "Striker")
+        ])
+    submit = SubmitField('create player')
